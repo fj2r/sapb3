85b7ff39b6c8b2c6223c6d919dbceeda2633625d
fj2r <fjr.rousseau@gmail.com>
2016-04-23 00:31:11 +0200

    Added files via upload

---
+++ layout_bundle.php
@@ -0,0 +1,17 @@
+<?php
+
+////////////////////////////* Appel du moteur de templates Twig*////////////////
+ 
+ include_once ('inc/initTwig.inc.php');
+ //////////////////////////////////Fin de l'appel du moteur de template/////////
+
+
+    
+$template = $twig->loadTemplate('layout_bundle.twig'); //on va chercher le template associé
+echo $template->render(array(
+    
+    )); //on envoie la variable au template
+
+
+    
+    
